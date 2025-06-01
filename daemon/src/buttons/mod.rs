57195a3127e3c8626a69d82dd99969a04759758f
cAttte <26514199+cAttte@users.noreply.github.com>
2025-06-01 14:15:25 -0300

    feat(buttons): add swap_mouse

--- daemon/src/buttons/mod.rs
+++ daemon/src/buttons/mod.rs
@@ -1,9 +1,10 @@
 use serde::Serialize;
 pub mod flip_screen;
 pub mod lock_mouse;
+pub mod swap_mouse;
 pub mod win_key;
 
-pub const ALL: &'static [Button] = &[win_key::BUTTON, flip_screen::BUTTON, lock_mouse::BUTTON];
+pub const ALL: &'static [Button] = &[flip_screen::BUTTON, lock_mouse::BUTTON, swap_mouse::BUTTON, win_key::BUTTON];
 
 #[derive(Serialize)]
 pub struct Button {
---
+++ daemon/src/buttons/swap_mouse.rs
@@ -0,0 +1,13 @@
+use windows::Win32::UI::Input::KeyboardAndMouse::SwapMouseButton;
+use windows::Win32::UI::WindowsAndMessaging::{GetSystemMetrics, SM_SWAPBUTTON};
+use macros::button;
+
+#[button(desc = "Swap mouse buttons")]
+fn swap_mouse(_: bool) -> Result<(), &'static str> {
+    unsafe {
+        let currently_normal = GetSystemMetrics(SM_SWAPBUTTON) == 0;
+        let _ = SwapMouseButton(currently_normal);
+    }
+
+    Ok(())
+}
